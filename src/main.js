e21d9bdb2ac8ff97c9573a7d2c9d89c5d61ce8aa
QuackityDuck <84103520+Navaneeth-Git@users.noreply.github.com>
2025-08-09 07:19:41 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 */
 
 (() => {
-  const  = document.getElementById('game');
+  const canvas = document.getElementById('game');
   const ctx = canvas.getContext('2d');
 
   // DPI scaling for crisp rendering
